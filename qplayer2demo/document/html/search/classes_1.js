49ac0c946335ad447bea261a9037b83b889c0d8c
mengwangchao <903064748@qq.com>
2024-01-02 10:51:23 +0800

    新增接口文档

---
+++ qplayer2demo/document/html/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['playerbasemessageloopqueueadapter_0',['PlayerBaseMessageLoopQueueAdapter',['../class_q_media_1_1_player_base_message_loop_queue_adapter.html',1,'QMedia']]]
+];
---
+++ qplayer2demo/document/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['add_5fstream_5felement_0',['add_stream_element',['../class_q_media_1_1_q_media_model_builder.html#a90836629b648b383e44e9850ea18e22c',1,'QMedia::QMediaModelBuilder']]],
+  ['add_5fsubtitle_5felement_1',['add_subtitle_element',['../class_q_media_1_1_q_media_model_builder.html#a58e355726427d0819827fac252cf717f',1,'QMedia::QMediaModelBuilder']]]
+];
